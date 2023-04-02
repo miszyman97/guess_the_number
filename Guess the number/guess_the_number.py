e9d4f48c0fec41570d9f19b7c4c736138725c327
miszyman <mszymanowski97@gmail.com>
2023-04-02 13:49:10 +0200

    Finished game ver.3

--- Guess the number/guess_the_number.py
+++ Guess the number/guess_the_number.py
@@ -1,24 +1,30 @@
 from random import randint
-"""Get number from user.
 
-Try until user gives a proper number.
 
-:rtype: None
-"""
+def guess_the_nuber():
+    """Get number from user.
 
-guess = False
-number = randint(1, 100)
+    Try until user gives a proper number.
 
-while guess != number:
-    try:
-        guess = int(input('Enter the number: '))
+    :return: None
+    """
 
-        if guess < number:
-            print('Too small!')
-        elif guess > number:
-            print('too big!')
-        else:
-            print('You win!')
-    except ValueError:
-        print('That is not a number!')
-        continue
+    guess = False
+    number = randint(1, 100)
+
+    while guess != number:
+        try:
+            guess = int(input('Enter the number: '))
+
+            if guess < number:
+                print('Too small!')
+            elif guess > number:
+                print('too big!')
+            else:
+                print('You win!')
+        except ValueError:
+            print('That is not a number!')
+            continue
+
+
+guess_the_nuber()
